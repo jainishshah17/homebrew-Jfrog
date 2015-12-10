2fbbcf440b4b75149524956c2df209a0b54bba01
Jainish Shah <Jainishshah@yahoo.com>
2015-12-10 13:57:39 -0800

    Update artifactory-cli-go.rb

--- artifactory-cli-go.rb
+++ artifactory-cli-go.rb
@@ -1,13 +1,21 @@
 class ArtifactoryCliGo < Formula
     desc "Artifactory CLI provides a command line interface for uploading and downloading artifacts to and from Artifactory."
-    homepage "https://github.com/JFrogDev/artifactory-cli-go"
-    url "https://bintray.com/artifact/download/jfrog/artifactory-cli-go/1.2.0/artifactory-cli-mac-386/art"
-    sha256 "79f3084291a4d54cfbf72dc1bbd16e14097077aa52897ed9b4e6fcf448758776"
+    homepage "https://www.jfrog.com/"
+    url "https://github.com/JFrogDev/artifactory-cli-go/archive/1.2.0.zip"
+    version "1.2.0"
+    sha256 "805888fb3d61e4558aa47ec5cbd39d89004f2130ec3fa5252260eb34782f10e6"
+    
+    depends_on "go"
     def install
+        ENV["GOPATH"] = buildpath
+        ENV["GOBIN"] = buildpath
+        ENV["GO15VENDOREXPERIMENT"] = "1"
+        (buildpath/"src/github.com/JFrogDev/artifactory-cli-go/").install Dir["*"]
+        system "go", "build", "-v", "github.com/JFrogDev/artifactory-cli-go/art/"
         bin.install "art"
-        bin.install_symlink libexec/"bin/art"
     end
+    
     test do
-        system bin/"art", "--version"
+        system "#{bin}/art"
     end
 end
