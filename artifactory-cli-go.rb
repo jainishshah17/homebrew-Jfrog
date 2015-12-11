efc12f59dbc65d6a7c14e7c524c07e2d8dd17a9a
Jainish Shah <Jainishshah@yahoo.com>
2015-12-11 15:52:01 -0800

    Update artifactory-cli-go.rb

--- artifactory-cli-go.rb
+++ artifactory-cli-go.rb
@@ -1,6 +1,6 @@
 class ArtifactoryCliGo < Formula
-  desc "Artifactory CLI provides a command line interface for uploading and downloading artifacts to and from Artifactory."
-  homepage "https://www.jfrog.com/"
+  desc "Artifactory-CLI provides a command-line interface."
+  homepage "https://github.com/JFrogDev/artifactory-cli-go"
   url "https://github.com/JFrogDev/artifactory-cli-go/archive/1.2.0.tar.gz"
   sha256 "173a24e0b821c6ff2bb85fa4cbe733fa681a8284029b77387baf0fa5f2fcfc4e"
 
@@ -16,7 +16,8 @@ class ArtifactoryCliGo < Formula
   end
 
   test do
-    system "#{bin}/art"
+    actual = pipe_output("#{bin}/art upload")
+    expected = "The --url option is mandatory\n"
+    assert_equal expected, actual
   end
-
 end
